ca0fe456d52648a12a7897ae7f57c3a18850c6d8
Hendrik 'T4cC0re' Meyer <github@t4cc0.re>
2017-11-06 05:46:36 +0100

    Decided to move the `xPreserve` methods into a separate interface to avoid breaking 3rd party implementations of `Archiver`

--- archiver.go
+++ archiver.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 )
 
-// Archiver represent a archive format
-type Archiver interface {
+// PreservableArchiver represent a archive format
+type PreservableArchiver interface {
 	// Match checks supported files
 	Match(filename string) bool
 	// Make makes an archive file on disk.
@@ -26,6 +26,20 @@ type Archiver interface {
 	OpenPreserve(source, destination string) error
 	// Read reads an archive from a Reader while preserving uid/gid.
 	ReadPreserve(input io.Reader, destination string) error
+}
+
+// Archiver represent a archive format
+type Archiver interface {
+	// Match checks supported files
+	Match(filename string) bool
+	// Make makes an archive file on disk.
+	Make(destination string, sources []string) error
+	// Open extracts an archive file on disk.
+	Open(source, destination string) error
+	// Write writes an archive to a Writer.
+	Write(output io.Writer, sources []string) error
+	// Read reads an archive from a Reader.
+	Read(input io.Reader, destination string) error
 }
 
 // SupportedFormats contains all supported archive formats
