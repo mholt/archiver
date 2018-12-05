8a7aa424aafa1777fe1b84010a569b151fde5292
Matthew Holt <mholt@users.noreply.github.com>
2018-12-05 15:37:05 -0700

    build.bash: Remove BSD and 32-bit, arm64 Linux builds
    They are hardly ever downloaded

--- build.bash
+++ build.bash
@@ -6,12 +6,8 @@ set -ex
 export CGO_ENABLED=0
 
 cd cmd/arc
-GOOS=linux   GOARCH=386   go build -o ../../builds/arc_linux_386
 GOOS=linux   GOARCH=amd64 go build -o ../../builds/arc_linux_amd64
 GOOS=linux   GOARCH=arm   go build -o ../../builds/arc_linux_arm7
-GOOS=linux   GOARCH=arm64 go build -o ../../builds/arc_linux_arm64
 GOOS=darwin  GOARCH=amd64 go build -o ../../builds/arc_mac_amd64
 GOOS=windows GOARCH=amd64 go build -o ../../builds/arc_windows_amd64.exe
-GOOS=freebsd GOARCH=amd64 go build -o ../../builds/arc_freebsd_amd64
-GOOS=openbsd GOARCH=amd64 go build -o ../../builds/arc_openbsd_amd64
 cd ../..
