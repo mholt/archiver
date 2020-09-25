61cb1e9ebc38629193da9d980a8380308579910c
AJ ONeal <coolaj86@gmail.com>
2020-09-25 17:23:12 -0600

    manual ARM builds for RPi 4, Windows, armv6

--- build.bash
+++ build.bash
@@ -5,9 +5,12 @@ set -ex
 
 export CGO_ENABLED=0
 
-cd cmd/arc
-GOOS=linux   GOARCH=amd64 go build -o ../../builds/arc_linux_amd64
-GOOS=linux   GOARCH=arm   go build -o ../../builds/arc_linux_arm7
-GOOS=darwin  GOARCH=amd64 go build -o ../../builds/arc_mac_amd64
-GOOS=windows GOARCH=amd64 go build -o ../../builds/arc_windows_amd64.exe
-cd ../..
+pushd cmd/arc
+GOOS=linux   GOARCH=amd64         go build -o ../../builds/arc_linux_amd64
+GOOS=linux   GOARCH=arm64         go build -o ../../builds/arc_linux_arm64
+GOOS=linux   GOARCH=arm           go build -o ../../builds/arc_linux_armv7
+GOOS=linux   GOARCH=arm   GOARM=6 go build -o ../../builds/arc_linux_armv6
+GOOS=darwin  GOARCH=amd64         go build -o ../../builds/arc_mac_amd64
+GOOS=windows GOARCH=amd64         go build -o ../../builds/arc_windows_amd64.exe
+GOOS=windows GOARCH=arm           go build -o ../../builds/arc_windows_armv7.exe
+popd
