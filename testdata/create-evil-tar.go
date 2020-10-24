279e87c3b65ffdff6555e9d819ec0cace9db14c3
AJ ONeal <coolaj86@gmail.com>
2020-10-24 16:47:52 -0600

    use proper mode and type flags

--- testdata/create-evil-tar.go
+++ testdata/create-evil-tar.go
@@ -2,34 +2,35 @@ package main
 
 import (
 	"archive/tar"
+	"fmt"
 	"log"
 	"os"
 	"time"
 )
 
 func main() {
-	// Create a buffer to write our archive to.
-	fw, err := os.Create("double-evil.tar")
+	// Create a file to write our archive to.
+	tarname := "double-evil.tar"
+	fw, err := os.Create(tarname)
 	if nil != err {
 		log.Fatal(err)
 		return
 	}
 
 	// Create a new tar archive.
-	w := tar.NewWriter(fw)
+	tw := tar.NewWriter(fw)
 
 	// Write the evil symlink, it points outside of the target directory
-	h := &tar.Header{
+	hdr := &tar.Header{
 		Name:     "bad/file.txt",
-		Typeflag: 2,
+		Mode:     0644,
+		Typeflag: tar.TypeSymlink,
 		Linkname: "../../badfile.txt",
 		ModTime:  time.Now(),
 	}
-
-	err = w.WriteHeader(h)
-
-	if err != nil {
+	if err := tw.WriteHeader(hdr); err != nil {
 		log.Fatal(err)
+		return
 	}
 
 	// Write safe files to the archive.
@@ -41,28 +42,30 @@ func main() {
 		{"bad/file.txt", "Mwa-ha-ha"},
 	}
 	for _, file := range files {
-		h := &tar.Header{
-			Name:     file.Name,
-			Typeflag: 0,
-			Size:     int64(len(file.Body)),
-			ModTime:  time.Now(),
+		hdr := &tar.Header{
+			Name:    file.Name,
+			Mode:    0644,
+			Size:    int64(len(file.Body)),
+			ModTime: time.Now(),
 		}
-		err := w.WriteHeader(h)
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = w.Write([]byte(file.Body))
 
-		if err != nil {
+		if err := tw.WriteHeader(hdr); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		if _, err := tw.Write([]byte(file.Body)); err != nil {
 			log.Fatal(err)
 		}
 	}
 
 	// Close the in-memory archive so that it writes trailing data
-	err = w.Close()
+	err = tw.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("Wrote %s\n", tarname)
+
 	// close the on-disk archive so that it flushes all bytes
 	if err = fw.Close(); err != nil {
 		log.Fatal(err)
