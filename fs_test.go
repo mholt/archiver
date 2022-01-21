a20bf160d90e5f1961359da7541ab9ad45ef46b2
Tamás Gulácsi <tgulacsi@users.noreply.github.com>
2022-01-21 18:00:23 +0100

    Add ExampleEmbed for ArchiveFS.Stream usage (#312)
    * Add ExampleEmbed for ArchiveFS.Stream usage
    
    * Add missing testdata
    
    Co-authored-by: Tamás Gulácsi <tamas@gulacsi.eu>

--- fs_test.go
+++ fs_test.go
@@ -1,6 +1,16 @@
 package archiver
 
-import "testing"
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"net/http"
+	"path"
+	"testing"
+)
 
 func TestPathWithoutTopDir(t *testing.T) {
 	for i, tc := range []struct {
@@ -27,4 +37,36 @@ func TestPathWithoutTopDir(t *testing.T) {
 			t.Errorf("Test %d (input=%s): Expected '%s' but got '%s'", i, tc.input, tc.expect, actual)
 		}
 	}
+}
+
+//go:generate zip testdata/test.zip go.mod
+
+//go:embed testdata/test.zip
+var testZIP []byte
+
+func ExampleEmbed() {
+	fsys := ArchiveFS{
+		Stream: io.NewSectionReader(bytes.NewReader(testZIP), 0, int64(len(testZIP))),
+		Format: Zip{},
+	}
+	// You can serve the contents in a web server:
+	http.Handle("/static", http.StripPrefix("/static",
+		http.FileServer(http.FS(fsys))))
+
+	// Or read the files using fs functions:
+	dis, err := fsys.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, di := range dis {
+		fmt.Println(di.Name())
+		b, err := fs.ReadFile(fsys, path.Join(".", di.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(bytes.Contains(b, []byte("require (")))
+	}
+	// Output:
+	// go.mod
+	// true
 }
